edb3bfbd7100c7dd95231cc6f8517b5e441f4be8
Ihor Sokolov <sihorjs@gmail.com>
2026-05-27 15:22:10 +0300

    add readme in ukraininan

--- pdf_editor/models/document_type.py
+++ pdf_editor/models/document_type.py
@@ -2,11 +2,14 @@ from enum import StrEnum
 
 
 class DocumentType(StrEnum):
+    PASSPORT = "Паспорт"
+    LABOURER_TICKET = "Трудова книжка"
     MILITARY_TICKET = "Військовий"
     OFFICER_CARD = "Офіцерське"
 
 
 document_max_pages = {
+    DocumentType.PASSPORT: 9,
     DocumentType.MILITARY_TICKET: 17,
     DocumentType.OFFICER_CARD: 10,
 }
